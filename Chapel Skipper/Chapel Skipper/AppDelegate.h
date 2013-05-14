329ef2e5ded8491fb48c9d3193f6952586a9a7f3
Richard Simpson <richardleesimpson@gmail.com>
2013-05-13 21:43:08 -0500

    Added initial project.

---
+++ Chapel Skipper/Chapel Skipper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Chapel Skipper
+//
+//  Created by Richard Simpson on 5/13/13.
+//  Copyright (c) 2013 Richard Simpson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
